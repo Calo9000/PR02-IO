5af8656cb0b19c379ac0080ebf28263d825d8007
calo <carlos.roberto.kruse@gmail.com>
2021-10-12 13:35:05 -0600

    se terminaron algoritmos pd y greedy

--- bstgreedy.h
+++ bstgreedy.h
@@ -13,6 +13,7 @@ int max(float p[], int n){
 	return t;
 }
 
+// n es cantidad de llaves, p es probabilidad de las llaves de acuerdo al orden de prioridad, r es arreglo raiz que se modifica
 void bstGreedy(int n, float p[N],  int R[N][N])
 {
 	
@@ -28,49 +29,11 @@ void bstGreedy(int n, float p[N],  int R[N][N])
 			if(j>0 && i<=j){
 				int s = j-i+1;
 				float T[s];
-				printf("%d, %d\n", i, j);
 				for(int k= 0; k<=j-i; k++){
 					T[k] = p[i+k-1];
-					printf("%.2f ", T[k]);
 				}
-				printf("\ns: %d ", s);
-				printf("MAX: %d ", max(T,s));
 				R[i-1][j] = max(T,s)+i;
-				printf("\n");
 			}
 		}
 	}
-
-
-}
-
-
-int main()
-{
-
-	//int n = 5;
-	int n = 4;
-	
-	//float p[] = {0.3, 0.2, 0.1, 0.15, 0.25};
-	float p[] = {0.18, 0.32, 0.39, 0.11};
-
-	//	EL ORDEN DE LAS PROBABILIDADES ES EL ORDEN DE PRIORIDAD DE LAS LLAVES
-
-
-	int R[N][N];
-
-	bstGreedy(n, p, R);
-	
-
-	printf("\n");
-	for(int i=0;i<n+1;i++){
-		for(int j = 0; j<n+1; j++){
-			printf("%d\t",R[i][j]);
-		}
-		printf("\n");
-	}
-
-
-
-	return 0;
 }
--- bstpd.h
+++ bstpd.h
@@ -3,29 +3,46 @@
 #define N 100
 #define inf 9999
 
-
-void bstPD(int n, float W[N][N], float A[N][N], float R[N][N], float P[N])
+// inicializa y rellena las matrices root y average. n es cantidad de llaves, p es un arreglo con la probabilidad de cada llave ordenado segun prioridad
+void bstPD(int n, float p[N],  int R[N][N], float A[N][N])
 {
-	int i,j,r,l;
-	float q;
-	for(i=1;i<=n+1;i++){
-		A[i][i-1]=0;
-		W[i][i-1]=0;
+	
+
+	for(int i=0;i<n+1;i++){
+		for(int j = 0; j<n+1; j++){
+			R[i][j]=0;
+			A[i][j]=0;
+		}
 	}
 
-	for(l=1;l<=n;l++){
-		for(i=1;i<=(n-l+1);i++){
-			j=i+l-1;
-			W[i][j]=W[i][j-1]+P[j];
-			A[i][j]=inf;
-			for(r=i;r<=j;r++){
-				q=A[i][r-1]+A[r+1][j]+W[i][j];
-				if(q<A[i][j]){
-					A[i][j]=q;
-					R[i][j]=r;
+
+	for(int j = 0; j<n+1;j++){
+		for(int ii = 0; ii<n+1; ii++){
+			int i = n-ii;
+			if(i==j){
+				A[i][j] = 0;
+			}
+			else if(j==(i+1)){
+				A[i][j] = p[i];
+				R[i][j] = i+1;
+			}
+			else if(j>(i+1)){
+				float B[n];
+				float c = 0;
+				float m = inf;
+				for(int k = i; k<j; k++){
+					c+=p[k];
 				}
+				for(int k = i; k<j; k++){
+					float x = A[i][k]+A[k+1][j]+c;
+					if(m>x){
+						m = x;
+						R[i][j] = k+1;
+					}
+				}
+				A[i][j] = m;
 			}
 		}
 	}
-}
 
+}
